86aadcd69af90aca736e04b2f30e3accc7b3f613
Christian C. Del Moro <christian.delmoro@protonmail.com>
2025-06-11 19:14:42 -0300

    wip: adds atlas personal blog template from tailawesome.com

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,32 @@
-import "./style.css";
-
 function global() {
   return {
     isMobileMenuOpen: false,
     isDarkMode: false,
-    themeInit: function() {
+    themeInit() {
       if (
-        localStorage.getItem("theme") === "dark" ||
+        localStorage.theme === "dark" ||
         (!("theme" in localStorage) &&
           window.matchMedia("(prefers-color-scheme: dark)").matches)
       ) {
-        localStorage.setItem("theme", "dark");
+        localStorage.theme = "dark";
         document.documentElement.classList.add("dark");
         this.isDarkMode = true;
       } else {
-        localStorage.setItem("theme", "light");
+        localStorage.theme = "light";
         document.documentElement.classList.remove("dark");
         this.isDarkMode = false;
       }
     },
-    themeSwitch: function() {
-      if (localStorage.getItem("theme") === "dark") {
-        localStorage.setItem("theme", "light");
+    themeSwitch() {
+      if (localStorage.theme === "dark") {
+        localStorage.theme = "light";
         document.documentElement.classList.remove("dark");
         this.isDarkMode = false;
       } else {
-        localStorage.setItem("theme", "dark");
+        localStorage.theme = "dark";
         document.documentElement.classList.add("dark");
         this.isDarkMode = true;
       }
     },
   };
 }
-
-
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/pages/index.html',
+        blog: './src/pages/blog.html',
+        post: './src/pages/post.html',
+        uses: './src/pages/uses.html',
+        contact: './src/pages/contact.html'
+      }
+    }
+  }
 });
